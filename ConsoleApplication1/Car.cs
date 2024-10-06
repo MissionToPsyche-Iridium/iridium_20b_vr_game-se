1973ad0c6947f2cedee0a715da6e936adf27616a
Kev Nangcas <knangcas@asu.edu>
2024-10-05 22:44:05 -0500

    Playing around with C#

---
+++ ConsoleApplication1/Car.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApplication1
+{
+    public class Car
+    {
+        private string color;
+        
+        public string getColor() {return color;}
+        
+        public void setColor(string color) {this.color = color;}
+
+        public Car()
+        {
+            color = "red";
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApplication1
+
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //this is a comment
+            Console.WriteLine("What's your name?");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hi " + name);
+
+            string[] cars = { "Ford Mustang", "Toyota Supra", "Chevrolet Corvette" };
+            //string is in lowercase, it's also a primitive
+            Console.WriteLine("I like the " + cars[0]);
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+            //very similar to java. (and js, and others)
+            
+            Car car = new Car();
+            Console.WriteLine("My car's color is " + car.getColor());
+
+
+
+
+        }
+    }
+}
