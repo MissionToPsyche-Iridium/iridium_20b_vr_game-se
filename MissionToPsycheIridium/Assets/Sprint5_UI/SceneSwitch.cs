f3358ea516a3205f33d6066c1909f04f20c6b1e8
knangcas <knangcas@asu.edu>
2025-01-20 16:00:40 -0600

    US165 Task 173 Coded disclaimer to show up when clicking "start game" or "event mode". Also added a back button.

--- MissionToPsycheIridium/Assets/Sprint5_UI/SceneSwitch.cs
+++ MissionToPsycheIridium/Assets/Sprint5_UI/SceneSwitch.cs
@@ -5,11 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitch : MonoBehaviour
 {
-  
+    [SerializeField] GameObject disclaimer;
+    GameObject canvas;
 
+    void Start()
+    {
+        canvas = GameObject.Find("MainMenuCanvas");
+        disclaimer.SetActive(false);
+    }
+
+    public void startGamePushed()
+    {
+        disclaimer.SetActive(true);
+        canvas.SetActive(false);
+    }
     public void switchToGame()
     {
+        
         SceneManager.LoadScene("MainGame");
+       
+    }
+
+    public void goBack()
+    {
+        disclaimer.SetActive(false);
+        canvas.SetActive(true);
     }
 
     public void quitGame()
