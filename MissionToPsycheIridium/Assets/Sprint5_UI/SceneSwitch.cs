48f3dfb730e98c86ce6fd7ca04394b7d8c0cafa9
knangcas <knangcas@asu.edu>
2025-01-20 16:10:08 -0600

    US165 TASK 201 tested this button in test build, confirmed working

--- MissionToPsycheIridium/Assets/Sprint5_UI/SceneSwitch.cs
+++ MissionToPsycheIridium/Assets/Sprint5_UI/SceneSwitch.cs
@@ -8,17 +8,22 @@ public class SceneSwitch : MonoBehaviour
     [SerializeField] GameObject disclaimer;
     GameObject canvas;
 
+    //this starts at the launch of scene
     void Start()
     {
         canvas = GameObject.Find("MainMenuCanvas");
         disclaimer.SetActive(false);
     }
 
+    //start game being pushed button
+    //activates disclaimer "page"
     public void startGamePushed()
     {
         disclaimer.SetActive(true);
         canvas.SetActive(false);
     }
+
+    //switches scene to game scene
     public void switchToGame()
     {
         
@@ -26,12 +31,16 @@ public class SceneSwitch : MonoBehaviour
        
     }
 
+
+    //this code is for going back when on the disclaimer page
     public void goBack()
     {
         disclaimer.SetActive(false);
         canvas.SetActive(true);
     }
 
+
+    //this code quits the game. This has been confirmed to work in the test build. 
     public void quitGame()
     {
         Application.Quit();
