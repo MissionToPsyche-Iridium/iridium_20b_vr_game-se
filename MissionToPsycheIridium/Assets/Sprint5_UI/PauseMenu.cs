7069cbd63d47798b64c74a429d5bc0f24132cddf
sray45 <shannonray147@gmail.com>
2025-01-21 21:33:25 -0500

    US175 Task#179: Coding VR to pause the game.

---
+++ MissionToPsycheIridium/Assets/Sprint5_UI/PauseMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject wristUI;
+    public bool activeWristUI = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DisplayWristUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PauseButtonPressed(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            DisplayWristUI();
+    }
+
+    public void DisplayWristUI()
+    {
+        if (activeWristUI)
+        {
+            wristUI.SetActive(false);
+            activeWristUI = false;
+            Time.timeScale = 1; 
+        }
+        else if (!activeWristUI)
+        {
+            wristUI.SetActive(true);
+            activeWristUI = true;
+            Time.timeScale = 0;
+        }
+    }
+}
