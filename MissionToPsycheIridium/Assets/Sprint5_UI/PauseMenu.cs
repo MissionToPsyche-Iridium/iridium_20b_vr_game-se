a924ae1ba2077a3668dd945bf111980489c95b8a
sray45 <shannonray147@gmail.com>
2025-01-22 00:50:28 -0500

    US175 Task#179 Fix and add to code to pause the game in VR

--- MissionToPsycheIridium/Assets/Sprint5_UI/PauseMenu.cs
+++ MissionToPsycheIridium/Assets/Sprint5_UI/PauseMenu.cs
@@ -1,52 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
-    public GameObject wristUI;
-    public bool activeWristUI = true;
+    public GameObject pauseMenu; 
+    public InputActionProperty pressPause;
+    public Text timerText;                
+    public Button resumeButton;           
 
+    private bool isPaused = false;         // Tracks when game is paused
+    private float gameTime = 0f;           // The game timer
+    private float timeScaleBeforePause;    // Stores timer before pausing the game
 
     // Start is called before the first frame update
     void Start()
     {
-        DisplayWristUI();
+        //Hide pause menu when game starts
+        pauseMenu.SetActive(false);
+
+        //Set up listener for resume button
+        resumeButton.onClick.AddListener(ResumeGame);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    public void PauseButtonPressed(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-            DisplayWristUI();
-    }
-
-    public void DisplayWristUI()
-    {
-        if (activeWristUI)
+        // Update the game timer if the game is not paused
+        if (!isPaused)
         {
-            wristUI.SetActive(false);
-            activeWristUI = false;
-            Time.timeScale = 1; 
-        }
-        else if (!activeWristUI)
-        {
-            wristUI.SetActive(true);
-            activeWristUI = true;
-            Time.timeScale = 0;
+            gameTime += Time.deltaTime;
+            UpdateTimerText();
         }
     }
 
-    public void RestartGame()
+    private void OnEnable()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        pressPause.action.performed += DisplayPause;
+    }
+
+    private void OnDisable()
+    {
+        pressPause.action.performed -= DisplayPause;
+    }
+
+    private void DisplayPause(InputAction.CallbackContext context)
+    {
+        if (isPaused)
+        {
+            ResumeGame(); // Resume the game if it's currently paused
+        }
+        else
+        {
+            PauseGame(); // Pause the game if it's currently running
+        }
+    }
+
+    private void PauseGame()
+    {
+        isPaused = true;
+        pauseMenu.SetActive(true); // Show the pause menu
+
+        // Stop the game timer by setting timeScale to 0
+        timeScaleBeforePause = Time.timeScale; // Save the current timeScale
+        Time.timeScale = 0f;
+    }
+
+    private void ResumeGame()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false); // Hide the pause menu
+
+        // Restore the game timer 
+        Time.timeScale = timeScaleBeforePause;
+    }
+
+    private void UpdateTimerText()
+    {
+        // Update the timer text 
+        timerText.text = $"Time: {gameTime:F2} seconds";
     }
 
 }
