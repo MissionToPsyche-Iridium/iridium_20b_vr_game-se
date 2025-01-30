cf5809e4dd0151540838fab6a2f090af67630fa0
sray45 <shannonray147@gmail.com>
2025-01-30 15:48:08 -0500

    US211 Task#215 Ensure timer is always visible to player.

---
+++ MissionToPsycheIridium/Assets/UIVisibility.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIVisibility : MonoBehaviour
+{
+    public Transform cameraTransform;
+
+    void LateUpdate()
+    {
+        transform.position = cameraTransform.position + cameraTransform.forward * 2f; 
+        transform.rotation = Quaternion.LookRotation(transform.position - cameraTransform.position);
+    }
+}
