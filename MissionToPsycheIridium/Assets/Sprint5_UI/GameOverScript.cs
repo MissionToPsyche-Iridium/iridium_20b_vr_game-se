922827c6d4f0fbae692af2a3f983ddb754667e6c
knangcas <knangcas@asu.edu>
2025-01-31 17:26:03 -0600

    US217 Task 222 added BGM and adjusted gameover and credits so that credits loads from gameover so BGM remains constant

--- MissionToPsycheIridium/Assets/Sprint5_UI/GameOverScript.cs
+++ MissionToPsycheIridium/Assets/Sprint5_UI/GameOverScript.cs
@@ -9,16 +9,26 @@ public class GameOverScript : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] TextMeshProUGUI scoreNumber;
+    [SerializeField] GameObject credits;
+    [SerializeField] GameObject gameOver;
     
     void Start()
     {
+        credits.SetActive(false);
+        gameOver.SetActive(true);
         scoreNumber.text = ScoreItem.getScore().ToString();
+    }
+
+    public void playButtonSound()
+    {
+        GetComponent<AudioSource>().Play();
     }
 
     public void restartGameButton()
     {
         //enter code to load main game scene here
         Debug.Log("restart button pressed");
+        playButtonSound();
         SceneManager.LoadScene("MainGame");
     }
 
@@ -26,7 +36,16 @@ public class GameOverScript : MonoBehaviour
     {
         //enter code to load credits scene here
         Debug.Log("exit button pressed");
-        SceneManager.LoadScene("Credits");
+        playButtonSound();
+        credits.SetActive(true);
+        gameOver.SetActive(false);
+        
+    }
+
+    public void mainMenuButton()
+    {
+        playButtonSound();
+        SceneManager.LoadScene("MainMenu");
     }
 
 
