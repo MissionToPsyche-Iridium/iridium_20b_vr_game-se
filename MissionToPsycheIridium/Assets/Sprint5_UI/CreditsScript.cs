9b6801b3efe4b6aec883c31dea3d02097bd08077
knangcas <knangcas@asu.edu>
2025-01-31 17:32:45 -0600

    US217 Task 222 Added BGM to credits scene. Also fixed a bug where ray tracers would not reach main menu button in game over screen. Also added sound buttons.

--- MissionToPsycheIridium/Assets/Sprint5_UI/CreditsScript.cs
+++ MissionToPsycheIridium/Assets/Sprint5_UI/CreditsScript.cs
@@ -8,7 +8,9 @@ public class CreditsScript : MonoBehaviour
   
     public void backToMainMenu()
     {
+
         //Code to go back to main menu here
+        GetComponent<AudioSource>().Play();
         Debug.Log("main menu button pressed");
         SceneManager.LoadScene("MainMenu");
     }
