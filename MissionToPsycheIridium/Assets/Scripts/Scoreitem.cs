f92783f9fd70702ed45de72a3f4c74ff89255d5f
knangcas <knangcas@asu.edu>
2025-02-26 13:16:47 -0600

    us 263 task 273 started coding the summary screen

--- MissionToPsycheIridium/Assets/Scripts/Scoreitem.cs
+++ MissionToPsycheIridium/Assets/Scripts/Scoreitem.cs
@@ -6,12 +6,18 @@ public static class ScoreItem
 {
 
     public static int Score = 0;
+    public static int GoldScore = 0;
+    public static int SilverScore = 0;
+    public static int IronScore = 0;
+    public static int NickelScore = 0;
+    public static int ResearchScore = 0;
 
     public static void increaseScore(int n)
     {
         Score = Score + n;
     }
 
+    //gameover scene calls this to get score
     public static int getScore()
     {
         return Score;
